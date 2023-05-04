886f2914a4397747912841da14f52eb71183390e
3120005043_张俊鸿 <1017328759@qq.com>
2023-05-04 17:58:51 +0800

    exp3_a

---
+++ experiments3/exp_a/src/main/java/cn/itcast/countducks/MainActivity.java
@@ -0,0 +1,123 @@
+package cn.itcast.countducks;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private MyBroadcastReceiverOne one;
+    private MyBroadcastReceiverTwo two;
+    private MyBroadcastReceiverThree three;
+    private ImageView iv_horn;
+    private TextView tv_left_content, tv_one, tv_two, tv_three;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        registerReceiver();
+        init();
+    }
+    private void init() {
+        iv_horn = findViewById(R.id.iv_horn);
+        tv_left_content = findViewById(R.id.tv_left_content);
+        tv_one = findViewById(R.id.tv_one);
+        tv_two = findViewById(R.id.tv_two);
+        tv_three = findViewById(R.id.tv_three);
+        iv_horn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_left_content.setVisibility(View.VISIBLE);
+                iv_horn.setClickable(false);  //设置喇叭图片为不可点击的状态
+                Intent intent = new Intent();
+                intent.setAction("Count_Ducks");      // 定义广播的事件类型
+                sendOrderedBroadcast(intent, null);  // 发送有序广播
+//                MyBroadcastReceiverThree receiver = new MyBroadcastReceiverThree();
+//                sendOrderedBroadcast(intent,null,receiver, null, 0, null, null); // 发送有序广播
+            }
+        });
+    }
+    private void registerReceiver() {
+        // 动态注册MyBroadcastReceiverOne广播
+        one = new MyBroadcastReceiverOne();
+        IntentFilter filter1 = new IntentFilter();
+        filter1.setPriority(10);
+        filter1.addAction("Count_Ducks");
+        registerReceiver(one, filter1);
+        // 动态注册MyBroadcastReceiverTwo广播
+        two = new MyBroadcastReceiverTwo();
+        IntentFilter filter2 = new IntentFilter();
+        filter2.setPriority(20);
+        filter2.addAction("Count_Ducks");
+        registerReceiver(two, filter2);
+        // 动态注册MyBroadcastReceiverThree广播
+        three = new MyBroadcastReceiverThree();
+        IntentFilter filter3 = new IntentFilter();
+        filter3.setPriority(6);
+        filter3.addAction("Count_Ducks");
+        registerReceiver(three, filter3);
+    }
+
+    private int num=0; // 存放序号的变量
+    class MyBroadcastReceiverOne extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            tv_one.setVisibility(View.VISIBLE);
+            num=num+1;
+            tv_one.setText(num+"");
+            Log.i("BroadcastReceiverOne", "广播接收者One,接收到了广播事件");
+            delay();
+        }
+    }
+    class MyBroadcastReceiverTwo extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            tv_two.setVisibility(View.VISIBLE);
+            num=num+1;
+            tv_two.setText(num+"");
+            Log.i("BroadcastReceiverTwo", "广播接收者Two,接收到了广播事件");
+//            abortBroadcast(); //拦截有序广播
+//            Log.i("MyBroadcastReceiverTwo", "我是广播接收者Two，广播被我终结了");
+            // 跳转
+            intent = new Intent(context, NewActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+            Log.i("BroadcastReceiverTwo", "广播接收者Two跳转到新页面");
+            delay();
+        }
+    }
+    class MyBroadcastReceiverThree extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            tv_three.setVisibility(View.VISIBLE);
+            num=num+1;
+            tv_three.setText(num + "");
+            Log.i("BroadcastReceiverThree", "广播接收者Three,接收到了广播事件");
+            delay();
+        }
+    }
+    /**
+     * 延迟500毫秒
+     */
+    private void delay(){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(one);
+        unregisterReceiver(two);
+        unregisterReceiver(three);
+    }
+}
